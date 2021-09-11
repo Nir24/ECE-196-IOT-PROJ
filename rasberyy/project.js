123dc6aecdf06393ecbac1d22470b93283a9a050
Nir24 <12155314+Nir24@users.noreply.github.com>
2021-09-10 21:20:08 -0700

    Update project.js

--- rasberyy/project.js
+++ rasberyy/project.js
@@ -2,16 +2,7 @@ const spawn = require("child_process").spawn;
 
 var firebase = require("firebase/app");
 require("firebase/database");
-const firebaseConfig = {
-  apiKey: "AIzaSyBNR3iNuDVUOo_BIRb8FUiTMjtiAQZEiVs",
-  authDomain: "ece196-sp21-proj.firebaseapp.com",
-  databaseURL: "https://ece196-sp21-proj-default-rtdb.firebaseio.com",
-  projectId: "ece196-sp21-proj",
-  storageBucket: "ece196-sp21-proj.appspot.com",
-  messagingSenderId: "53304839199",
-  appId: "1:53304839199:web:7743d3d5b51336a944992a",
-  measurementId: "G-YPT6WF7F1P",
-};
+
 
 firebase.initializeApp(firebaseConfig);
 db = firebase.database();
